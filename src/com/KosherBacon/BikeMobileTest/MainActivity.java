803dfbf6ddbea26b6ca6fa209f93f241a4170c2d
Joshua Kahn <j.harris.kahn@gmail.com>
2015-03-12 23:47:40 -0400

    Adding button functionality

--- src/com/KosherBacon/BikeMobileTest/MainActivity.java
+++ src/com/KosherBacon/BikeMobileTest/MainActivity.java
@@ -2,18 +2,35 @@ package com.KosherBacon.BikeMobileTest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        EditText username = (EditText) findViewById(R.id.usernameField);
-        EditText password = (EditText) findViewById(R.id.passwordField);
+        /*
+        EditText fields from the main.xml page to later grab data from
+         */
+        final EditText usernameField = (EditText) findViewById(R.id.usernameField);
+        final EditText passwordField = (EditText) findViewById(R.id.passwordField);
+
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = usernameField.getText().toString();
+                String pass = passwordField.getText().toString();
+                if (user.isEmpty() || pass.isEmpty()) {
+                    Toast.makeText(getBaseContext(), "Complete all fields!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
