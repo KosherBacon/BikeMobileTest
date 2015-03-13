fc20a39a95e5fdf631b321313abfd0e0a1cca7db
Joshua Kahn <j.harris.kahn@gmail.com>
2015-03-13 16:44:30 -0400

    Working on UI

--- src/com/KosherBacon/BikeMobileTest/BikeChooserActivity.java
+++ src/com/KosherBacon/BikeMobileTest/BikeChooserActivity.java
@@ -1,12 +1,7 @@
 package com.KosherBacon.BikeMobileTest;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.*;
 
 /**
