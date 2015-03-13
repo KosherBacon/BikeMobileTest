f35d425bdf1dc4c5a7a1740622d5c92e73535118
Joshua Kahn <j.harris.kahn@gmail.com>
2015-03-12 23:36:57 -0400

    Working on the login page

--- src/com/KosherBacon/BikeMobileTest/MainActivity.java
+++ src/com/KosherBacon/BikeMobileTest/MainActivity.java
@@ -2,6 +2,7 @@ package com.KosherBacon.BikeMobileTest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
     /**
@@ -11,5 +12,8 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        EditText username = (EditText) findViewById(R.id.usernameField);
+        EditText password = (EditText) findViewById(R.id.passwordField);
     }
 }
