c94cecc78cdea9c321e58297c4d5b1beb9898c51
Joshua Kahn <j.harris.kahn@gmail.com>
2015-03-13 16:51:29 -0400

    Using Lorem Ipsum generator

--- src/com/KosherBacon/BikeMobileTest/BikeChooserActivity.java
+++ src/com/KosherBacon/BikeMobileTest/BikeChooserActivity.java
@@ -3,6 +3,7 @@ package com.KosherBacon.BikeMobileTest;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.*;
+import net._01001111.text.LoremIpsum;
 
 /**
  * Created by jkahn on 3/12/15.
@@ -16,11 +17,14 @@ public class BikeChooserActivity extends Activity {
 
         final ListView bikeList = (ListView) findViewById(R.id.bikeList);
 
+        LoremIpsum lorem = new LoremIpsum();
+
         Bike[] bikes = new Bike[100];
         for (int i = 0; i < 100; i++) {
             bikes[i] = new Bike();
             bikes[i].setName("Roadster");
             bikes[i].setManufacturer("Schwinn");
+            bikes[i].setDescription(lorem.paragraphs(2, true));
         }
         BikeArrayAdapter bikeAdapter = new BikeArrayAdapter(getApplicationContext(), R.layout.bike_selector_row, bikes);
         bikeList.setAdapter(bikeAdapter);
